4731189c25654197503f4330a5ea314931350bac
sulemanbasit <83623379+sulemanbasit@users.noreply.github.com>
2021-05-09 18:09:11 -0600

    For simple threshold

--- Image Filtering/main1.py
+++ Image Filtering/main1.py
@@ -1,4 +1,20 @@
 import cv2
 import numpy as np
 
-print ("Hello World")
+img = cv2.imread('numbers.jpg', 0)
+cv2.imshow('before', img)
+cv2.waitKey(0)
+threshold_num = [55, 90, 150]
+for i in threshold_num:
+    img2 = img.copy()
+    for row in img2:
+        for j in range(len(row)):
+            if row[j] > i:
+                row[j] = 255
+            else:
+                row[j] = 0
+    cv2.imshow('after {} threshold'.format(i), img2)
+    cv2.waitKey(0)
+
+
+
